9dcb709bbca1c9ee4c4080a3bd665c5e04e811fa
Jason Feimster <jasonfeimster@gmail.com>
2026-05-14 08:04:46 -0400

    Add product data and site constants

---
+++ lib/constants/navigation.ts
@@ -0,0 +1,8 @@
+export const NAVIGATION = [
+  { label: 'Pricing', href: '/pricing' },
+  { label: 'Use Cases', href: '/use-cases' },
+  { label: 'Tools', href: '/tools' },
+  { label: 'Demo', href: '/demo' },
+  { label: 'Privacy', href: '/privacy' },
+  { label: 'Terms', href: '/terms' }
+];
